bc2c0f770e97fc0bff28d6f6ede7105ab5b2b8ad
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-06 11:10:36 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//console.log("hello");
+
+//const person = require("./person");
+
+// console.log(person);
+// console.log(person.name);
+const Person = require("./person");
+
+const person1 = new Person("John Doe", 30);
+
+person1.greeting();
